a0aa63ff1c294cfe94ab8bd51b397342e1cc14ab
Vin-it-9 <vineetshinde2559@gmail.com>
2025-12-20 03:30:16 +0530

    Removed `ComparisonSection`, `CTACards`, and related styles/constants as part of homepage cleanup

--- src/components/Homepage/index.js
+++
@@ -1,9 +0,0 @@
-// Export all homepage components from a single entry point
-export { default as HeroSection } from './HeroSection';
-export { default as CTACards } from './CTACards';
-export { default as ComparisonSection } from './ComparisonSection';
-export { default as KeyFeatures } from './KeyFeatures';
-export { default as SuccessStories } from './SuccessStories';
-
-// Also export constants if needed elsewhere
-export * from './constants';
